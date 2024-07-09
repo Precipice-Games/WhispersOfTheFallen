7651ec39d1346b94a7274dfcc3b4390f5e2d985d
kolhuang <kol.huang1@marist.edu>
2024-07-09 10:13:37 -0400

    Brian Dungeon Movement Fix

--- DungeonBrian.gd
+++ DungeonBrian.gd
@@ -1,33 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const speed = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+	
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
-func _process(delta):
-	var velocity = Vector2.ZERO 
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
+	var direction = Vector3.ZERO
+	
 	
