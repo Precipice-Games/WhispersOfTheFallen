9187d5dc8c4587819761a17a9b12ae117f0d6fa2
AidanBattaglia <aidan.battaglia1@marist.edu>
2024-07-11 11:53:31 -0400

    no more break down

--- Melee_Hitbox.gd
+++ Melee_Hitbox.gd
@@ -6,11 +6,14 @@ var attack_per_sec = 2
 
 
 func activate():
+	await get_tree().create_timer(0.5).timeout #start up time
 	$Detector.disabled = false
+	deactivate()
+	
 	
 func deactivate():
+	await get_tree().create_timer(0.5).timeout #attack lenght time
 	$Detector.disabled = true
 
 func _physics_process(delta):
-	pass
-	rotate(delta * deg_to_rad(360) * attack_per_sec) 
+	rotate(delta * deg_to_rad(360) * attack_per_sec)
