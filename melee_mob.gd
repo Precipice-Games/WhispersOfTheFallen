d417291ecdd787c958c7dc983f2cd663f237bead
AidanBattaglia <aidan.battaglia1@marist.edu>
2024-07-11 13:43:51 -0400

    Ranged and melee done

--- melee_mob.gd
+++ melee_mob.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
+
+var speed = 150
+var player_chase = false
+var player_null = null
+#@onready var Melee_Hitbox = $Melee_Hitbox
 @export var player:CharacterBody2D
 
 # var player_direction = player.position - self.position 
-
+func _physics_process(delta):
+	if player_chase:
+		position += (player.position - self.position).normalized() * speed * delta
+		move_and_collide(Vector2(0,0)) 
+		
+	#if Melee_Hitbox._is_colliding():
+		#velocity += Melee_Hitbox.get_push_vector() * delta * 400
 
 func _on_attack_area_body_entered(_body):
 	print("Attacking")
@@ -11,5 +22,6 @@ func _on_attack_area_body_entered(_body):
 	
 
 func _on_detection_area_body_entered(_body):
-	pass # Chase the player
+	player_null =  $"../DungeonBrian"
+	player_chase = true
 
