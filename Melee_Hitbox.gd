a211d04f1435982d0e3b8cbcf6257c5600613bc0
AidanBattaglia <aidan.battaglia1@marist.edu>
2024-07-11 11:29:20 -0400

    more progress

--- Melee_Hitbox.gd
+++ Melee_Hitbox.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 var activation_time:float = 0
 var recovery_time:float = 0
+var attack_per_sec = 2
+
 
 func activate():
 	$Detector.disabled = false
 	
 func deactivate():
 	$Detector.disabled = true
+
+func _physics_process(delta):
+	pass
+	rotate(delta * deg_to_rad(360) * attack_per_sec) 
--- melee_mob.gd
+++ melee_mob.gd
@@ -4,9 +4,13 @@ extends CharacterBody2D
 @export var Melee_Scene : PackedScene = preload("res://Melee_Hitbox.tscn")
 var Melee_Hitbox = Melee_Scene.instantiate()
 
-var player_direction = player.position - self.position 
+# var player_direction = player.position - self.position 
+
+
+
 
 func _on_attack_area_body_entered(body):
+	print("Attacking")
 	Melee_Hitbox.activate() # attack player
 	
 
