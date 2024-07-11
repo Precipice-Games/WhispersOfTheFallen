5b0732f4b281f8be1dede15ec884accca5415339
AidanBattaglia <aidan.battaglia1@marist.edu>
2024-07-11 10:45:01 -0400

    work in progress

--- melee_mob.gd
+++ melee_mob.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
+@export var player:CharacterBody2D
+@export var Melee_Scene : PackedScene = preload("res://Melee_Hitbox.tscn")
+var Melee_Hitbox = Melee_Scene.instantiate()
+
+var player_direction = player.position - self.position 
 
 func _on_attack_area_body_entered(body):
-	pass # attack player
-
+	Melee_Hitbox.activate() # attack player
+	
 
 func _on_detection_area_body_entered(body):
 	pass # Chase the player
-
-
-
-
 
 
