7780a504b2a8d61a903dbced032bea8d21f91561
kolhuang <kol.huang1@marist.edu>
2024-07-12 16:53:49 -0400

    MAGIC ACTUALLY FIXED!!!!!

--- Test_Arena.gd
+++ Test_Arena.gd
@@ -1,8 +1,1 @@
 extends Node2D
-
-@onready var pause_menu = $DungeonBrian/Camera2D/PauseMenu
-var paused = false
-
-#func _is_colliding():
-	#var areas = get_overlapping_areas()
-	#return areas.size( ) > 0
